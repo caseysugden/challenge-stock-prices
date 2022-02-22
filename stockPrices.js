7ecc261ac0729ab5b68633d6744a18473a2c5dd6
Casey Sugden <casey.sugden@gmail.com>
2022-02-22 10:04:00 -0700

    Updated comments and syntax

--- stockPrices.js
+++ stockPrices.js
@@ -1,12 +1,15 @@
 function best(stockPrices) {
+    // Initialize 'best' to 0
     let best = 0;
     for (let i = 0; i < stockPrices.length; i++) {
+        // Initialize nested for loop to 'i'
         for (let j = i; j < stockPrices.length; j++) {
+            // Check difference between array elements in the order they were given
             if (getDifference(stockPrices[i], stockPrices[j]) > best) {
                 best = getDifference(stockPrices[i], stockPrices[j]);
-            }
-        }
-    }
+            };
+        };
+    };
     return best;
 };
 
